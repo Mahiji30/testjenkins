4f696a71a1f0474cc34af39a80c0e5bf7f9aa317
Mahi Gupta <gupta.1234mahi@gmail.com>
2026-02-16 11:07:47 +0530

    Modified Hello Program

--- Helllo.java
+++ Helllo.java
@@ -1,5 +1,9 @@
 public class Helllo {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+        System.out.println("Sum of a and b is: " + sum);
+        System.out.println("Hello World!");
     }
 }
